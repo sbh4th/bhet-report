1e4cb503515dd1d79ffc751f9fa124ad7abbeea9
Sam Harper <sbh4th@gmail.com>
2024-04-10 16:17:26 -0400

    Add respiratory and bp results to table

--- code/etwfe-te-ap.R
+++ code/etwfe-te-ap.R
@@ -45,7 +45,10 @@ ap_data <- read_csv(here("data-clean",
 
 # data frames
 d_personal <- ap_data %>%
-  filter(PM25_exp_remove == 1) 
+  filter(PM25_exp_remove == 1) %>%
+  # drop households with duplicate values
+  distinct(hh_id, wave, PM25conc_exposureugm3, 
+           .keep_all= TRUE)
 
 d_bc <- ap_data %>%
   filter(bc_exp_remove == 1)
@@ -55,165 +58,28 @@ d_ind_24h <- ap_data %>%
   filter(!is.na(pm2.5_indoor_sensor_24h)) %>%
   # drop households with duplicate values
   distinct(hh_id, wave, pm2.5_indoor_sensor_24h, 
-           .keep_all= TRUE)
+           .keep_all= TRUE) 
 
 d_ind_seasonal <- ap_data %>% 
   filter(N_percent_indoor_seasonal_hs >= 0.2) %>% 
   distinct(hh_id, wave, pm2.5_indoor_seasonal_hs, 
            .keep_all= TRUE)
 
-# Function to estimate models
-estimate_etwfe <- function(outcome_vars, predictor_vars) {
-  models <- outcome_vars %>%
-    set_names() %>%
-    map(~ {
-      formula <- as.formula(paste(.x, "~", 
-        paste(predictor_vars, collapse = " + ")))
-      glm(formula, data = dresp, family = "binomial")
-    })
-  
-  return(models)
-}
-
-# Function to estimate models
-estimate_etwfe <- function(outcome, predictors, data) {
-  models <- predictors %>%
-    set_names() %>%
-    map(~ {
-      formula <- as.formula(paste(outcome, "~", 
-        paste(.x, collapse = " + ")))
-      glm(formula, data = data, cluster = ~v_id)
-    })
-  
-  return(models)
-}
-
-
 # basic DiD specification
 rhs_did <- c("treat:cohort_year_2019:year_2019",
   "treat:cohort_year_2019:year_2021", 
   "treat:cohort_year_2020:year_2021",
-  "treat:cohort_year_2021:year_2021", "cohort_year_2019",
+  "treat:cohort_year_2021:year_2021",
   "cohort_year_2020", "cohort_year_2021",
-  "year_2019", "year_2021")
+  "year_2021", "year_2019", "cohort_year_2019")
 
 # DiD plus covariates
 rhs_dida <- c(rhs_did, "hh_num", "ptc_smoking", 
   "outdoor_temp_24h", "outdoor_dew_24h")
 
-# gather estimates across models
-
-# personal PM
-# basic ETWFE model
-did_ppm <- fixest::feols(
-  as.formula(paste("PM25conc_exposureugm3 ~ ", 
-    paste(rhs_did, collapse= "+"))), 
-  data = d_personal, cluster = ~v_id)
-
-me_ppm <- slopes(
-  did_ppm,
-  newdata = subset(d_personal, treat==1),
-  variables = "treat",
-  by = "treat"
-  )
-
-# adjusted ETWFE model
-did_ppm <- fixest::feols(
-  as.formula(paste("PM25conc_exposureugm3 ~ ", 
-    paste(rhs_dida, collapse= "+"))), 
-  data = d_personal, cluster = ~v_id)
-
-# black carbon
-# basic ETWFE model
-did_pbc <- fixest::feols(
-  as.formula(paste("bc_exp_conc ~ ", 
-    paste(rhs_did, collapse= "+"))), 
-  data = d_personal, cluster = ~v_id)
-
-# adjusted ETWFE model
-did_ppm <- fixest::feols(
-  as.formula(paste("bc_exp_conc ~ ", 
-    paste(rhs_dida, collapse= "+"))), 
-  data = d_personal, cluster = ~v_id)
-
-
-# basic DiD
-logit_did <- estimate_logit_did(b_out, rhs_did)
-# with covariates
-logit_dida <- estimate_logit_did(b_out, rhs_dida)
-
-# estimate marginal effects (simple average)
-# basic DiD
-logit_me <- lapply(logit_did, marginaleffects::slopes, 
-  newdata = subset(dresp, treat==1), 
-  variables = "treat", by = "treat",
-  # make sure to use cluster-robust SEs
-  vcov = ~v_id)
-
-# adjusted DiD
-logit_mea <- lapply(logit_dida, marginaleffects::slopes, 
-  newdata = subset(dresp, treat==1), 
-  variables = "treat", by = "treat",
-  # make sure to use cluster-robust SEs
-  vcov = ~v_id)
-
-# grab estimates and SEs from DiD results
-did_t1 <- logit_me %>% {
-  tibble(
-    # table = 1,
-    outcome = names(logit_me),
-    est = map_dbl(., "estimate"),
-    stderror = map_dbl(., "std.error"),
-    ll = est - 1.96 * stderror,
-    ul = est + 1.96 * stderror,
-    ci = paste("(", round(ll, 2), ",",
-               round(ul, 2), ")")
-  )
-}
-
-# grab estimates and SEs from adjusted DiD results
-did_t2 <- logit_mea %>% {
-  tibble(
-    # table = 2,
-    # outcome = names(logit_me),
-    esta = map_dbl(., "estimate"),
-    stderrora = map_dbl(., "std.error"),
-    lla = esta - 1.96 * stderrora,
-    ula = esta + 1.96 * stderrora,
-    cia = paste("(", round(lla, 2), ",",
-               round(ula, 2), ")")
-  )
-}
-
-didt <- cbind(did_t1, did_t2) %>%
-  dplyr::select(outcome, est, ll, ul, esta, lla, ula) 
-
-didt$outcome = c("Any respiratory symptom", "Coughing",
-  "Phlegm", "Wheezing attacks", "Trouble breathing",
-  "Chest trouble")
-
-# write results table to dataset
-write_rds(didt, file = here("data-clean", 
-  "resp-did.rds"))
-
-kable(didt, digits = 2, 
-  col.names = c("Frequency of:", "Estimate", 
-  "LL", "UL", "Estimate", "LL", "UL")) %>% #, "latex", booktabs = T) %>%
-  kable_styling() %>%
-  add_header_above(c(" " = 1, 
-                     "Basic DiD" = 3, "Adjusted DiD" = 3))
-
-
-
-
-
-
-
-
-
-# Function to estimate generalized linear regression model for two different sets of predictors
+# Function to estimate ETWFE models and marginal linear regression model # effects across different outcomes
 estimate_etwfe <- function(
-    lhs, rhs1, rhs2, data, treatvar) {
+    lhs, rhs1, rhs2, data) {
   
   # Create model formula for model 1
   fml1 <- as.formula(paste(lhs, "~", 
@@ -228,22 +94,33 @@ estimate_etwfe <- function(
       etwfe1, newdata = subset(data, treat==1),
       variables = "treat", by = "treat")
     
-  # test for heterogeneity across treatment cohorts
+  # estimates across group-time treatment cohorts
   me_het1 <- marginaleffects::slopes(
       etwfe1, newdata = subset(data, treat==1),
       variables = "treat", 
-      by = c("cohort_year", "year"),
-      hypothesis = c("b1 - b2 = 0", "b1 - b3 = 0", 
+      by = c("cohort_year", "year"))
+  
+  # now test for heterogeneity
+  # for estimates using all 4 waves
+  if (nrow(me_het1)==4) {
+    # estimate the model contrast
+    me_het1_c <- hypotheses(me_het1, hypothesis = 
+      c("b1 - b2 = 0", "b1 - b3 = 0", 
                      "b1 - b4 = 0"))
-    
-  # get test and p-value from joint test
-  me_jt1 <- hypotheses(me_het1, joint = TRUE)
+    # get test and p-value from joint test
+    me_jt1 <- hypotheses(me_het1_c, joint = TRUE)
+  # for estimates using 3 waves
+  } else {
+    me_het1_c <- hypotheses(me_het1, hypothesis = 
+      c("b1 - b2 = 0"))
+    me_jt1 <- hypotheses(me_het1_c, joint = TRUE)
+  }
 
-  # Create model formula for model 1
+  # Create model formula for adjusted model
   fml2 <- as.formula(paste(lhs, "~", 
     paste(rhs2, collapse = " + ")))  
   
-  # Fit model for the second set of predictors
+  # Fit model for adjusted ETWFE
   etwfe2 <- fixest::feols(fml = fml2, 
     data = data, cluster = ~v_id)
   
@@ -252,16 +129,27 @@ estimate_etwfe <- function(
       etwfe2, newdata = subset(data, treat==1),
       variables = "treat", by = "treat")
   
-  # test for heterogeneity across treatment cohorts
+  # allow for heterogeneity across treatment cohorts
   me_het2 <- marginaleffects::slopes(
       etwfe2, newdata = subset(data, treat==1),
       variables = "treat", 
-      by = c("cohort_year", "year"),
-      hypothesis = c("b1 - b2 = 0", "b1 - b3 = 0", 
-                     "b1 - b4 = 0"))
+      by = c("cohort_year", "year"))
     
+  # now test for heterogeneity
+  # for estimates using all 4 waves
+  if (nrow(me_het2)==4) {
+    # estimate the model contrast
+    me_het2_c <- hypotheses(me_het2, hypothesis = 
+      c("b1 - b2 = 0", "b1 - b3 = 0", 
+                     "b1 - b4 = 0"))
   # get test and p-value from joint test
-  me_jt2 <- hypotheses(me_het2, joint = TRUE)
+    me_jt2 <- hypotheses(me_het2_c, joint = TRUE)
+  # for estimates using 3 waves
+  } else {
+    me_het2_c <- hypotheses(me_het2, hypothesis = 
+      c("b1 - b2 = 0"))
+    me_jt2 <- hypotheses(me_het2_c, joint = TRUE)
+  }
   
   # Return a list containing both models
   return(list(e1 = etwfe1, me_1 = me_etwfe1, 
@@ -274,9 +162,156 @@ m_ppm <- estimate_etwfe(lhs="PM25conc_exposureugm3",
   rhs1=rhs_did, rhs2=rhs_dida, 
   data=d_personal)
 
+# ETWFE and adjusted ETWFE models for black carbon
 m_pbc <- estimate_etwfe(lhs="bc_exp_conc", 
   rhs1=rhs_did, rhs2=rhs_dida, 
   data=d_bc)
+
+# ETWFE and adjusted ETWFE models for indoor daily
+m_i24 <- estimate_etwfe(lhs="pm2.5_indoor_sensor_24h", 
+  rhs1=rhs_did, rhs2=rhs_dida, 
+  data=subset(d_ind_24h, cohort_year_2019!=1))
+
+# ETWFE and adjusted ETWFE models for indoor seasonal
+m_is <- estimate_etwfe(lhs="pm2.5_indoor_seasonal_hs", 
+  rhs1=rhs_did, rhs2=rhs_dida, 
+  data=subset(d_ind_seasonal, cohort_year_2019!=1))
+
+# tables of results by outcome
+ap_table_p <- bind_rows(m_ppm$me_1, m_ppm$me_2) %>% 
+  mutate(category = "Personal", outcome = "PM2.5")
+
+ap_table_bc <- bind_rows(m_pbc$me_1, m_pbc$me_2) %>% 
+  mutate(category = "Personal", 
+         outcome = "Black carbon")
+
+ap_table_i24 <- bind_rows(m_i24$me_1, m_i24$me_2) %>% 
+  mutate(category = "Indoor", 
+         outcome = "Daily")
+
+ap_table_is <- bind_rows(m_is$me_1, m_is$me_2) %>% 
+  mutate(category = "Indoor", 
+         outcome = "Seasonal")
+
+# put all subtables together
+ap_table1 <- bind_rows(ap_table_p, ap_table_bc,
+  ap_table_i24, ap_table_is) %>%
+  mutate(model = rep(c(1,2),times=4)) %>%
+  select(model, estimate, conf.low, 
+    conf.high, category, outcome) %>%
+  relocate(category, outcome) %>%
+  mutate(ci = paste("(", round(conf.low, 2), ", ",
+    round(conf.high, 2), ")", sep="")) %>%
+    #me = paste(round(estimate,2),
+     # " (", round(conf.low, 2), ", ",
+    #round(conf.high, 2), ")", sep="")) %>%
+  select(-conf.low, -conf.high) %>%
+  pivot_wider(names_from = model, values_from = 
+    c(estimate, ci), names_vary = "slowest")
+
+# now grab outdoor estimates
+ap_table2 <- read_csv(here("data-clean", 
+  "DID_air_pollution.csv")) %>%
+  filter(`Category` == "Outdoor") %>%
+  rename_at(c("Category", "Pollutant", "Estimate"), 
+    .funs = tolower) %>%
+  rename("outcome" = `pollutant`) %>%
+  mutate(model = rep(c(1,2), times = 2),
+    ci = paste("(", `CI_low`, ", ",
+    `CI_upper`, ")", sep="")) %>%
+  select(-Effect, -CI_low, -CI_upper) %>%
+  relocate(category, outcome) %>%
+  pivot_wider(names_from = model, values_from = 
+    c(estimate, ci), names_vary = "slowest")
+
+ap_table <- bind_rows(ap_table1, ap_table2)
+
+# write table to data
+write_rds(ap_table, file = here("data-clean", 
+  "ap-did-table.rds"))
+
+kable(ap_table, digits = 2,
+  col.names = c(" ", " ", "Marginal effect", "(95% CI)", 
+   "Marginal effect", "(95% CI)"), #"latex", booktabs = T,
+  linesep = "") %>%
+  kable_styling(full_width = F) %>%
+  collapse_rows(columns = 1:2, valign = "top") %>% 
+  pack_rows("Air pollution", 1, 4) %>%
+  add_header_above(c(" " = 2, 
+                    "DiD" = 2, "Adjusted DiD" = 2))
+
+modelsummary(list("DiD" = m_ppm$me_1, 
+  "Adjusted DiD" = m_ppm$me_2),
+  gof_omit = ".*",
+  estimate = "{estimate} [{conf.low}, {conf.high}]",
+  statistic = NULL)
+
+
+modelsummary(
+  ap_panels,
+  shape = "rbind",
+  gof_omit = ".*",
+  estimate = "{estimate} [{conf.low}, {conf.high}]",
+  statistic = NULL,
+  fmt = 2)
+  )
+
+ap_panels <- list(
+  "Personal: PM2.5" = list(
+    "DiD" = m_ppm$me_1, 
+    "Adjusted DiD" = m_ppm$me_2),
+  "Personal: Black carbon" = list(
+    "DiD" = m_pbc$me_1, 
+    "Adjusted DiD" = m_pbc$me_2),
+  "Indoor: daily" = list(
+    "DiD" = m_i24$me_1,
+    "Adjusted DiD" = m_i24$me_2),
+  "Indoor: seasonal" = list(
+    "DiD" = m_is$me_1,
+    "Adjusted DiD" = m_is$me_2)
+  )
+  
+
+modelsummary(
+  ap_panels,
+  shape = "rbind",
+  gof_omit = ".*",
+  estimate = "{estimate} [{conf.low}, {conf.high}]",
+  statistic = NULL,
+  fmt = 2)
+
+# traditional TWFE models
+p_twfe <- fixest::feols(
+  PM25conc_exposureugm3 ~ treat | v_id + year,
+  cluster = ~v_id, data = d_personal)
+p_twfe_me <- slopes(
+  
+)
+
+# Goodman-Bacon decomposition
+library(bacondecomp)
+
+df_bacon <- bacon(mipm ~ treat,
+                  data = vla,
+                  id_var = "v_id",
+                  time_var = "year")
+
+coef_bacon <- sum(df_bacon$estimate * df_bacon$weight)
+print(paste("Weighted sum of decomposition =", round(coef_bacon, 4)))
+
+fit_tw <- lm(vla ~ post + factor(state) + factor(year), 
+             data = bacondecomp::castle)
+print(paste("Two-way FE estimate =", round(fit_tw$coefficients[2], 4)))
+
+example_attgt <- att_gt(yname = "mipm",
+                        tname = "year",
+                        idname = "v_id",
+                        gname = "cy",
+                        # xformla = ~X,
+                        data = vla_cs
+                        )
+summary(example_attgt)
+aggte(example_attgt, type = "group")
 
 
 

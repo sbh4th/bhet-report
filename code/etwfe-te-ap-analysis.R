ff91b54cdc9980d7741a355107d0ea7c98ad37af
Sam Harper <sbh4th@gmail.com>
2024-04-15 20:59:03 -0400

    Add table of PM measures across seasons

---
+++ code/etwfe-te-ap-analysis.R
@@ -0,0 +1,399 @@
+#  program:  etwfe-te-ap-analysis.R
+#  task:     estimate ETWFE modesls for air pollution
+#  input:    various .rds files
+#  output:   various tables
+#  project:  BHET
+#  author:   sam harper \ 2024-04-15
+
+
+## 0 Load needed packages ----
+library(here)
+library(tidyverse)
+library(osfr)
+library(fixest)
+library(marginaleffects)
+library(modelsummary)
+library(kableExtra)
+
+
+## 1 read in clean air pollution datasets ----
+
+# personal PM2.5
+d_p <- read_rds(here("data-clean",
+  "ap-data-personal.rds"))
+
+# personal black carbon
+d_bc <- read_rds(here("data-clean",
+  "ap-data-bc.rds"))
+
+# indoor daily
+d_i24 <- read_rds(here("data-clean",
+  "ap-data-i24h.rds"))
+
+# indoor seasonal
+d_is <- read_rds(here("data-clean",
+  "ap-data-iseason.rds"))
+
+
+## 2 define a function to estimate the and adjusted 
+## ETWFE models, estimate the marginal effects 
+## (including heterogeneity) and put results in a list ----
+
+estimate_etwfe <- function(
+    lhs, rhs1, rhs2, data) {
+  
+  # Create model formula for model 1
+  fml1 <- as.formula(paste(lhs, "~", 
+    paste(rhs1, collapse = " + ")))
+  
+  # Fit model for the first set of predictors
+  etwfe1 <- fixest::feols(fml = fml1, 
+    data = data, cluster = ~v_id)
+  
+  # get marginal effects
+  me_etwfe1 <- marginaleffects::slopes(
+      etwfe1, newdata = subset(data, treat==1),
+      variables = "treat", by = "treat")
+    
+  # estimates across group-time treatment cohorts
+  me_het1 <- marginaleffects::slopes(
+      etwfe1, newdata = subset(data, treat==1),
+      variables = "treat", 
+      by = c("cohort_year", "year"))
+  
+  # now test for heterogeneity
+  # for estimates using all 4 waves
+  if (nrow(me_het1)==4) {
+    # estimate the model contrast
+    me_het1_c <- hypotheses(me_het1, hypothesis = 
+      c("b1 - b2 = 0", "b1 - b3 = 0", 
+                     "b1 - b4 = 0"))
+    # get test and p-value from joint test
+    me_jt1 <- hypotheses(me_het1_c, joint = TRUE)
+  # for estimates using 3 waves
+  } else {
+    me_het1_c <- hypotheses(me_het1, hypothesis = 
+      c("b1 - b2 = 0"))
+    me_jt1 <- hypotheses(me_het1_c, joint = TRUE)
+  }
+
+  # Create model formula for adjusted model
+  fml2 <- as.formula(paste(lhs, "~", 
+    paste(rhs2, collapse = " + ")))  
+  
+  # Fit model for adjusted ETWFE
+  etwfe2 <- fixest::feols(fml = fml2, 
+    data = data, cluster = ~v_id)
+  
+  # get marginal effects
+    me_etwfe2 <- marginaleffects::slopes(
+      etwfe2, newdata = subset(data, treat==1),
+      variables = "treat", by = "treat")
+  
+  # allow for heterogeneity across treatment cohorts
+  me_het2 <- marginaleffects::slopes(
+      etwfe2, newdata = subset(data, treat==1),
+      variables = "treat", 
+      by = c("cohort_year", "year"))
+    
+  # now test for heterogeneity
+  # for estimates using all 4 waves
+  if (nrow(me_het2)==4) {
+    # estimate the model contrast
+    me_het2_c <- hypotheses(me_het2, hypothesis = 
+      c("b1 - b2 = 0", "b1 - b3 = 0", 
+                     "b1 - b4 = 0"))
+  # get test and p-value from joint test
+    me_jt2 <- hypotheses(me_het2_c, joint = TRUE)
+  # for estimates using 3 waves
+  } else {
+    me_het2_c <- hypotheses(me_het2, hypothesis = 
+      c("b1 - b2 = 0"))
+    me_jt2 <- hypotheses(me_het2_c, joint = TRUE)
+  }
+  
+  # Return a list containing both models
+  return(list(e1 = etwfe1, me_1 = me_etwfe1,
+              meh_1 = me_het1, ht1 = me_jt1, 
+              e2 = etwfe2, me_2 = me_etwfe2,
+              meh_2 = me_het2, ht2 = me_jt2))
+}
+
+# covariates for basic ETWFE specification
+rhs_did <- c("treat:cohort_year_2019:year_2019",
+  "treat:cohort_year_2019:year_2021", 
+  "treat:cohort_year_2020:year_2021",
+  "treat:cohort_year_2021:year_2021",
+  "cohort_year_2020", "cohort_year_2021",
+  "year_2021", "year_2019", "cohort_year_2019")
+
+# ETWFE plus covariates
+rhs_dida <- c(rhs_did, "hh_num", "factor(smoking)", 
+  "outdoor_temp_24h", "outdoor_dew_24h")
+
+
+## 3 Estimate models for all AP outcomes ----
+# personal PM
+m_ppm <- estimate_etwfe(lhs="PM25conc_exposureugm3", 
+  rhs1=rhs_did, rhs2=rhs_dida, 
+  data=d_p)
+
+# ETWFE and adjusted ETWFE models for black carbon
+m_pbc <- estimate_etwfe(lhs="bc_exp_conc", 
+  rhs1=rhs_did, rhs2=rhs_dida, 
+  data=d_bc)
+
+# ETWFE and adjusted ETWFE models for indoor daily
+m_i24 <- estimate_etwfe(lhs="pm2.5_indoor_sensor_24h", 
+  rhs1=rhs_did, rhs2=rhs_dida, 
+  # restrict to cohorts treated after 2019
+  # in the absence of data in season 1
+  data=subset(d_i24, cohort_year_2019!=1))
+
+# ETWFE and adjusted ETWFE models for indoor seasonal
+m_is <- estimate_etwfe(lhs="pm2.5_indoor_seasonal_hs", 
+  rhs1=rhs_did, rhs2=rhs_dida, 
+  # restrict to cohorts treated after 2019
+  # in the absence of data in season 1
+  data=subset(d_is, cohort_year_2019!=1))
+
+# write model results to output folder
+write_rds(m_ppm, file = here("outputs/models",
+  "m_ppm.rds"))
+
+write_rds(m_pbc, file = here("outputs/models",
+  "m_pbc.rds"))
+
+write_rds(m_i24, file = here("outputs/models",
+  "m_i24.rds"))
+
+write_rds(m_is, file = here("outputs/models",
+  "m_is.rds"))
+
+
+## 3 Generate table of results for all AP outcomes ----
+
+# tables of results by outcome
+ap_table_p <- bind_rows(m_ppm$me_1, m_ppm$me_2) %>% 
+  mutate(category = "Personal", outcome = "PM2.5")
+
+ap_table_bc <- bind_rows(m_pbc$me_1, m_pbc$me_2) %>% 
+  mutate(category = "Personal", 
+         outcome = "Black carbon")
+
+ap_table_i24 <- bind_rows(m_i24$me_1, m_i24$me_2) %>% 
+  mutate(category = "Indoor", 
+         outcome = "Daily")
+
+ap_table_is <- bind_rows(m_is$me_1, m_is$me_2) %>% 
+  mutate(category = "Indoor", 
+         outcome = "Seasonal")
+
+# put all subtables together
+ap_table1 <- bind_rows(ap_table_p, ap_table_bc,
+  ap_table_i24, ap_table_is) %>%
+  mutate(model = rep(c(1,2),times=4)) %>%
+  select(model, estimate, conf.low, 
+    conf.high, category, outcome) %>%
+  relocate(category, outcome) %>%
+  mutate(ci = paste("(", round(conf.low, 2), ", ",
+    round(conf.high, 2), ")", sep="")) %>%
+  select(-conf.low, -conf.high) %>%
+  pivot_wider(names_from = model, values_from = 
+    c(estimate, ci), names_vary = "slowest")
+
+# now grab outdoor estimates
+# download from OSF
+aim_1 <- osf_retrieve_node("qsmbr")
+aim_1 %>%
+  osf_ls_files("Air pollution",
+    pattern = "DID_air_pollution.csv") %>%
+  osf_download(path = here("data-clean"),
+               conflicts = "overwrite")
+
+ap_table2 <- read_csv(here("data-clean", 
+  "DID_air_pollution.csv")) %>%
+  filter(`Category` == "Outdoor" & `Effect` !=
+           "DiD with S3") %>%
+  rename_at(c("Category", "Pollutant", "Estimate"), 
+    .funs = tolower) %>%
+  rename("outcome" = `pollutant`) %>%
+  mutate(model = rep(c(1,2), times = 2),
+    ci = paste("(", `CI_low`, ", ",
+    `CI_upper`, ")", sep="")) %>%
+  select(-Effect, -CI_low, -CI_upper) %>%
+  relocate(category, outcome) %>%
+  pivot_wider(names_from = model, values_from = 
+    c(estimate, ci), names_vary = "slowest")
+
+ap_table <- bind_rows(ap_table1, ap_table2)
+
+# write table to output folder
+write_rds(ap_table, file = here("outputs", 
+  "ap-etwfe-table.rds"))
+
+# html table for design
+# kable(ap_table, digits = 2,
+#   col.names = c(" ", " ", "Marginal effect", "(95% CI)", 
+#    "Marginal effect", "(95% CI)"), #"latex", booktabs = T,
+#   linesep = "") %>%
+#   kable_styling(full_width = F) %>%
+#   collapse_rows(columns = 1:2, valign = "top") %>% 
+#   pack_rows("Air pollution", 1, 4) %>%
+#   add_header_above(c(" " = 2, 
+#                     "DiD" = 2, "Adjusted DiD" = 2))
+
+
+## 4 Table of heterogeneous treatment effects ----
+
+# tables of results by outcome for personal
+ap_table_ph <- bind_rows(m_ppm$me_2, m_ppm$meh_2) %>% 
+  mutate(outcome = "PM2.5")
+
+ap_table_bch <- bind_rows(m_pbc$me_2, m_pbc$meh_2) %>% 
+  mutate(outcome = "Black carbon")
+
+aph_table <- bind_rows(ap_table_ph, ap_table_bch) %>%
+  select(outcome, estimate, conf.low, conf.high, 
+         cohort_year, year) %>%
+  mutate_at(vars(c(cohort_year,year)), ~ recode(., 
+         `2019` = "2019",
+         `2020` = "2020",
+         `2021` = "2021",
+         .missing = "All")) %>%
+  relocate(outcome, cohort_year, year) %>%
+  mutate(ci = paste("(", round(conf.low, 2), ", ",
+    round(conf.high, 2), ")", sep="")) %>%
+  select(-conf.low, -conf.high) %>%
+  pivot_wider(names_from = outcome, values_from = 
+    c(estimate, ci), names_vary = "slowest")
+
+# write table to data
+write_rds(aph_table, file = here("outputs", 
+  "ap-het-table.rds"))
+
+# tables of results by outcome for indoor
+ap_table_i24h <- bind_rows(m_i24$me_2, m_i24$meh_2) %>% 
+  mutate(outcome = "Daily")
+
+ap_table_ish <- bind_rows(m_is$me_2, m_is$meh_2) %>% 
+  mutate(outcome = "Seasonal")
+
+aph_table_i <- bind_rows(ap_table_i24h, ap_table_ish) %>%
+  select(outcome, estimate, conf.low, conf.high, 
+         cohort_year, year) %>%
+  mutate_at(vars(c(cohort_year,year)), ~ recode(., 
+         `2020` = "2020",
+         `2021` = "2021",
+         .missing = "All")) %>%
+  relocate(outcome, cohort_year, year) %>%
+  mutate(ci = paste("(", round(conf.low, 2), ", ",
+    round(conf.high, 2), ")", sep="")) %>%
+  select(-conf.low, -conf.high) %>%
+  pivot_wider(names_from = outcome, values_from = 
+    c(estimate, ci), names_vary = "slowest")
+
+# write table to data
+write_rds(aph_table_i, file = here("outputs", 
+  "ap-het-table-indoor.rds"))
+
+# html table for design
+# kable(aph_table, digits = 2,
+#   col.names = c("Cohort", "Year", "ATT", "(95% CI)", 
+#    "ATT", "(95% CI)"), #"latex", booktabs = T,
+#   linesep = "") %>%
+#   kable_styling(full_width = F) %>%
+#   # collapse_rows(columns = 1:2, valign = "top") %>% 
+#   add_header_above(c(" " = 2, 
+#                     "PM2.5" = 2, "Black carbon" = 2)) %>%
+#   footnote(symbol=c(paste("Joint test that all ATTs are equal: ", 
+#     "F(", m_ppm$ht1$df1, ", ", m_ppm$ht1$df2, ")= " , 
+#     round(m_ppm$ht1$statistic, digits=3), ", p= ", 
+#     round(m_ppm$ht1$p.value, digits=3)), 
+#     paste("Joint test that all ATTs are equal: ", 
+#           "F(", m_pbc$ht1$df1, ", ", m_pbc$ht1$df2, ")= " ,
+#           round(m_pbc$ht1$statistic, digits=3), ", p= ", 
+#           round(m_pbc$ht1$p.value, digits=3))), 
+#     footnote_as_chunk = T) 
+
+
+## 5 Impact of S3 air pollution data ----
+
+# read in indoor seasonal data plus season 3
+d_is3 <- read_rds(here("data-clean",
+  "ap-data-iseason-s3.rds"))
+
+# ETWFE model estimates including Season 3
+m_is_s3 <- fixest::feols(
+  pm2.5_indoor_seasonal_hs ~ treat:cohort_year_2020:year_2020 + 
+    treat:cohort_year_2020:year_2021 +
+    treat:cohort_year_2021:year_2021 + cohort_year_2020 + 
+    cohort_year_2021 + year_2021, 
+  cluster = ~v_id, data=subset(d_is3, 
+    cohort_year_2019!=1))
+
+# overall marginal effect
+m_is_s3_me <- slopes(
+  m_is_s3,
+  newdata = subset(d_is3, 
+    cohort_year_2019!=1 & treat==1),
+  variables = "treat", by = "treat")
+
+# heterogeneous ATTs
+m_is_s3_meh <- slopes(
+  m_is_s3,
+  newdata = subset(d_is3, 
+    cohort_year_2019!=1 & treat==1),
+  variables = "treat",
+  by = c("cohort_year", "year"))
+  
+
+# estimates without Season 3
+m_is_nos3 <- fixest::feols(
+  pm2.5_indoor_seasonal_hs ~ treat:cohort_year_2020:year_2020 + 
+    treat:cohort_year_2020:year_2021 +
+    treat:cohort_year_2021:year_2021 + cohort_year_2020 + 
+    cohort_year_2021 + year_2021, 
+  cluster = ~v_id, data=subset(d_is3, 
+    cohort_year_2019!=1 & wave!="S3"))
+
+# overall marginal effect
+m_is_nos3_me <- slopes(
+  m_is_nos3,
+  newdata = subset(d_is3, 
+    cohort_year_2019!=1 & wave!="S3" & treat==1),
+  variables = "treat", by = "treat")
+
+# heterogeneous ATTs
+m_is_nos3_meh <- slopes(
+  m_is_nos3,
+  newdata = subset(d_is3, 
+    cohort_year_2019!=1 & treat==1),
+  variables = "treat",
+  by = c("cohort_year", "year"))
+
+# Put together the table
+ap_ind_s3 <- bind_rows(m_is_s3_me , m_is_s3_meh) %>% 
+  mutate(outcome = "wS3")
+
+ap_ind_nos3 <- bind_rows(m_is_nos3_me, m_is_nos3_meh) %>% 
+  mutate(outcome = "woS3")
+
+ap_is3_table <- bind_rows(ap_ind_s3, ap_ind_nos3) %>%
+  select(outcome, estimate, conf.low, conf.high, 
+         cohort_year, year) %>%
+  mutate_at(vars(c(cohort_year,year)), ~ recode(., 
+         `2019` = "2019",
+         `2020` = "2020",
+         `2021` = "2021",
+         .missing = "All")) %>%
+  relocate(outcome, cohort_year, year) %>%
+  mutate(ci = paste("(", round(conf.low, 2), ", ",
+    round(conf.high, 2), ")", sep="")) %>%
+  select(-conf.low, -conf.high) %>%
+  pivot_wider(names_from = outcome, values_from = 
+    c(estimate, ci), names_vary = "slowest")
+
+# write table to data
+write_rds(ap_is3_table, file = here("outputs", 
+  "ap-is3_table.rds"))
---
+++ code/etwfe-te-ap-data.R
@@ -0,0 +1,178 @@
+#  program:  etwfe-te-ap-data.R
+#  task:     prepare datasets for air pollution outcomes
+#  input:    BHET_master_air_pollution.csv
+#  output:   d_personal.rds, d_bc.rds, d_ind_24h.rds
+#            d_ind_seasonal.rds, d_is_s3_add.rds
+#  project:  BHET
+#  author:   sam harper \ 2024-04-15
+
+
+## 0 Load needed packages ----
+library(here)
+library(tidyverse)
+library(osfr)
+library(fastDummies)
+library(modeldb)
+
+## 1 read in existing air pollution data from OSF ----
+# bring in air pollution data
+
+# read in data for Aim 1
+aim_1 <- osf_retrieve_node("qsmbr")
+aim_1 %>%
+  osf_ls_files("Air pollution",
+    pattern = "BHET_master_air_pollution.csv") %>%
+  osf_download(path = here("data-clean"),
+               conflicts = "overwrite")
+
+# read in air pollution data from OSF
+# restrict to specific variables
+ap_data <- read_csv(here("data-clean", 
+  "BHET_master_air_pollution.csv"),
+  col_select = c(ptc_id, hh_id, ID_VILLAGE, wave, 
+    PM25conc_exposureugm3, PM25_exp_remove, 
+    bc_exp_conc, bc_exp_remove,
+    usable_indoor_filter, bc_indoor_remove,
+    indoor_filter_type, indoor_filter_id, 
+    house_area, pm2.5_indoor_sensor_24h,
+    pm2.5_indoor_seasonal_hs, coal_ban_time,
+    N_percent_indoor_seasonal_hs, year,
+    ban_status_composite, ptc_smoking, hh_smoking, 
+    outdoor_temp_24h, outdoor_dew_24h, hh_num)) %>%
+  
+  # create year and cohort_year variables
+  mutate(year = if_else(wave=="S1", 2018, 
+      if_else(wave=="S2", 2019,
+        if_else(wave=="S4", 2021, 0))),
+    cohort_year = if_else(
+      ban_status_composite==1, 2019, 
+      if_else(ban_status_composite==2, 2020, 
+              if_else(ban_status_composite==3, 2021, 2022))),
+    treat = ifelse(year >= cohort_year, 1, 0),
+    cohort_year = ifelse(cohort_year == 2022,-Inf, 
+                         cohort_year)) %>%
+  # relabel last cohort year 
+  # treatment cohort dummies
+  add_dummy_variables(cohort_year, 
+    values=c(-Inf,2019,2020,2021), 
+    remove_original = F) %>%
+  # wave dummies
+  add_dummy_variables(year, 
+    values=c(2018,2019,2021), remove_original = F) %>%
+  
+  group_by(ID_VILLAGE) %>%
+  mutate(v_id = cur_group_id()) %>%
+  ungroup()
+
+
+## 2 create datasets for each outcome ----
+
+# personal exposure for PM2.5
+d_personal <- ap_data %>%
+  filter(PM25_exp_remove == 1) %>%
+  # smoking variable for consistency across outcomes
+  mutate(smoking = ptc_smoking) %>%
+  # drop households with duplicate values
+  distinct(hh_id, wave, PM25conc_exposureugm3, 
+           .keep_all= TRUE)
+
+# write to data-clean folder
+write_rds(d_personal, file = here("data-clean",
+ "ap-data-personal.rds"))
+
+# personal exposure for black carbon
+d_bc <- ap_data %>%
+  # smoking variable for consistency across outcomes
+  mutate(smoking = ptc_smoking) %>%
+  filter(bc_exp_remove == 1)
+
+# write to data-clean folder
+write_rds(d_bc, file = here("data-clean",
+ "ap-data-bc.rds"))
+
+# indoor exposure (daily)
+d_ind_24h <- ap_data %>% 
+  # keep usable values of indoor PM
+  filter(!is.na(pm2.5_indoor_sensor_24h)) %>%
+  # recode smoking variable to reflect household status
+  mutate(smoking = if_else(hh_smoking=="Smoking", 
+                      "Smoker", "Non-smoker")) %>%
+  # drop households with duplicate values
+  distinct(hh_id, wave, pm2.5_indoor_sensor_24h, 
+           .keep_all= TRUE)
+
+# write to data-clean folder
+write_rds(d_ind_24h, file = here("data-clean",
+ "ap-data-i24h.rds"))
+
+# indoor exposure (seasonal)
+d_ind_seasonal <- ap_data %>% 
+  filter(N_percent_indoor_seasonal_hs >= 0.2) %>%
+  # recode smoking variable to reflect household status
+  mutate(smoking = if_else(hh_smoking=="Smoking", 
+                      "Smoker", "Non-smoker")) %>%
+  distinct(hh_id, wave, pm2.5_indoor_seasonal_hs, 
+           .keep_all= TRUE)
+
+# write to data-clean folder
+write_rds(d_ind_seasonal, file = here("data-clean",
+ "ap-data-iseason.rds"))
+
+# indoor exposure (seasonal) including season 3
+# bring in season 3 seasonal data from OSF
+aim_1 %>%
+  osf_ls_files("Air pollution",
+    pattern = "indoor_pm_S3.csv") %>%
+  osf_download(path = here("data-clean"),
+               conflicts = "overwrite")
+
+# read in season 3 data
+d_ind_s3 <- read_csv(here("data-clean", 
+  "indoor_pm_S3.csv")) %>%
+  select(hh_id, wave, ID_VILLAGE, 
+    coal_ban_time, pm2.5_indoor_seasonal_hs, 
+    N_percent_indoor_seasonal_hs)
+
+# add to indoor seasonal
+d_is_s3_add <- d_ind_seasonal %>%
+  select(hh_id, wave, ID_VILLAGE,
+    coal_ban_time,
+    pm2.5_indoor_seasonal_hs, 
+    N_percent_indoor_seasonal_hs) %>%
+  bind_rows(d_ind_s3) %>%
+  filter(N_percent_indoor_seasonal_hs >= 0.2) %>%
+  mutate(year = if_else(wave=="S1", 2018, 
+      if_else(wave=="S2", 2019,
+      if_else(wave=="S3", 2020,
+      if_else(wave=="S4", 2021, 0)))),
+    ban_status_composite = case_when(
+      coal_ban_time == 2019 ~ 1, 
+      coal_ban_time == 2020 ~ 2, 
+      coal_ban_time == 2021 ~ 3, 
+      coal_ban_time == "Not Yet" ~ 0),
+    cohort_year = if_else(
+      ban_status_composite==1, 2019, 
+      if_else(ban_status_composite==2, 2020, 
+              if_else(ban_status_composite==3, 2021, 2022))),
+    treat = ifelse(year >= cohort_year, 1, 0),
+    cohort_year = ifelse(cohort_year == 2022,-Inf, 
+                         cohort_year)) %>%
+  # relabel last cohort year 
+  # treatment cohort dummies
+  add_dummy_variables(cohort_year, 
+    values=c(-Inf,2019,2020,2021), 
+    remove_original = F) %>%
+  # wave dummies
+  add_dummy_variables(year, 
+    values=c(2018,2019,2020,2021), 
+    remove_original = F) %>%
+  
+  group_by(ID_VILLAGE) %>%
+  mutate(v_id = cur_group_id()) %>%
+  ungroup()
+
+# write to data-clean folder
+write_rds(d_is_s3_add, file = here("data-clean",
+ "ap-data-iseason-s3.rds"))
+
+
